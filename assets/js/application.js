a6abc759319be278c15287dc9b3b6ad202080459
amitkaps <amitkaps@gmail.com>
2013-05-26 23:23:01 +0530

    Initial commit

---
+++ assets/js/application.js
@@ -0,0 +1,17 @@
+!function ($) {
+
+	$(function(){
+
+	// for dropdown
+	$('.dropdown-toggle').dropdown()
+	
+	// for carousel
+	$('#myCarousel').carousel({interval: 4000}) 
+	
+	// for collapse
+	$(".collapse").collapse()
+
+	})
+
+}(window.jQuery)
+	
